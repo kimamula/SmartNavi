df1c488df504cff309da2737daa723cef4751921
Kenji-Imamura <kenji_imamura@dwango.co.jp>
2016-06-11 09:29:39 +0900

    initialize component for native

--- index.android.js
+++ index.android.js
@@ -1,4 +1,12 @@
 import { AppRegistry } from 'react-native'
+import * as React from 'react';
 import SmartNavi from './src/SmartNavi'
+import router from './src/router';
 
-AppRegistry.registerComponent('SmartNavi', () => SmartNavi);
+class SmartNaviNative extends React.Component {
+    render() {
+        return <SmartNavi router={router} pathQuery={{ path: '' }} />;
+    }
+}
+
+AppRegistry.registerComponent('SmartNavi', () => SmartNaviNative);
