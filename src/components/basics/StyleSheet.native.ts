f812ab7e3c228b09589e5a0298218d8ca2e9993a
Kenji-Imamura <kenji_imamura@dwango.co.jp>
2016-06-11 09:29:52 +0900

    remove display for native

--- src/components/basics/StyleSheet.native.ts
+++ src/components/basics/StyleSheet.native.ts
@@ -1,2 +1,24 @@
 import { StyleSheet } from 'react-native';
-export default StyleSheet;
+
+namespace StyleSheetWrapper {
+    export function create<T>(style: T): T {
+        removeDisplay(style);
+        return StyleSheet.create(style);
+    }
+
+    function removeDisplay(style: any): void {
+        let remove = false;
+        Object.keys(style).forEach(key => {
+            if (typeof style[key] === 'object') {
+                removeDisplay(style[key]);
+            } else if (key === 'display') {
+                remove = true;
+            }
+        });
+        if (remove) {
+            delete style.display;
+        }
+    }
+}
+
+export default StyleSheetWrapper;
