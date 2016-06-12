d6a50649143d46fe8424bcd4d49e848cb2b66665
Kenji-Imamura <kenji_imamura@dwango.co.jp>
2016-06-12 23:34:10 +0900

    fix progress for iOS

--- src/components/basics/Progress.ios.tsx
+++ src/components/basics/Progress.ios.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react';
 import { ProgressViewIOS } from 'react-native';
 
-class Progress extends React.Component<void, void> {
+class Progress extends React.Component<void, Progress.State> {
+    constructor() {
+        super();
+        this.state = { progress: 0 };
+    }
+    componentDidMount(): void {
+        this.updateProgress();
+    }
     render(): JSX.Element {
-        return <ProgressViewIOS />;
+        return <ProgressViewIOS progress={this.getProgress()} />;
+    }
+
+    private updateProgress(): void {
+        setInterval(() => {
+            this.setState({ progress: this.state.progress + 0.01 })
+        }, 50);
+    }
+
+    private getProgress(): number {
+        return Math.sin(this.state.progress % Math.PI) % 1;
+    }
+}
+
+namespace Progress {
+    export interface State {
+        progress: number;
     }
 }
 
