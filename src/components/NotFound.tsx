f3c93761b95ba15af9b939766fc8edd86f062955
Kenji-Imamura <kenji_imamura@dwango.co.jp>
2016-06-13 10:06:52 +0900

    modify NotFound

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import Text from './basics/Text';
+import View from './basics/View';
+import StyleSheet from './basics/StyleSheet';
 
 class NotFound extends React.Component<void, void> {
     render(): JSX.Element {
-        return <Text>NotFound</Text>;
+        return <View style={styles.root}>
+            <Text style={styles.title}>NotFound</Text>
+        </View>;
     }
 }
+
+const styles = StyleSheet.create({
+    root: {
+        backgroundColor: '#e9eaed',
+        flex: 1
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 10
+    }
+});
 
 export default NotFound;
